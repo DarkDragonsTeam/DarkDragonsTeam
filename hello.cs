37ce951227ea70f187fb6b6be908292351a1a05f
DarkDragons Team <106605841+DarkDragonsTeam@users.noreply.github.com>
2022-05-31 17:57:30 -0700

    Create a C# file (Hello from DarkDragons).

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+  
+
+namespace HelloWorldApp {
+    class App {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, from DarkDragons❤️");
+            
+            Console.ReadKey();
+        }
+    }
+}
